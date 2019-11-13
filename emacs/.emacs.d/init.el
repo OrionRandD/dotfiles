2dfa4458b7677e6fd0914391cfc6eaf58a725b9b
Vagner Rener <vagnerrener@gmail.com>
2019-11-13 13:41:41 -0300

    emacs good :)
    	modified:   emacs/.emacs.d/init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -405,21 +405,19 @@
 
    (company-ac-setup)
 
- (custom-set-faces
-     '(company-preview
-       ((t (:foreground "darkgray" :underline t))))
-     '(company-preview-common
-       ((t (:inherit company-preview))))
-     '(company-tooltip
-       ((t (:background "lightgray" :foreground "black"))))
-     '(company-tooltip-selection
-       ((t (:background "steelblue" :foreground "white"))))
-     '(company-tooltip-common
-       ((((type x)) (:inherit company-tooltip :weight bold))
-        (t (:inherit company-tooltip))))
-     '(company-tooltip-common-selection
-       ((((type x)) (:inherit company-tooltip-selection :weight bold))
-        (t (:inherit company-tooltip-selection)))))
+ 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-preview ((t (:foreground "darkgray" :underline t))))
+ '(company-preview-common ((t (:inherit company-preview))))
+ '(company-tooltip ((t (:background "lightgray" :foreground "black"))))
+ '(company-tooltip-common ((((type x)) (:inherit company-tooltip :weight bold)) (t (:inherit company-tooltip))))
+ '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
+ '(company-tooltip-selection ((t (:background "steelblue" :foreground "white"))))
+ '(fringe ((t (:background "unspecified-bg")))))
 ;; company:1 ends here
 
 ;; [[file:~/.dotfiles/emacs/.emacs.d/init.org::*org-bullets][org-bullets:1]]
@@ -2974,3 +2972,28 @@ bbdb-popup-target-lines  1
 (straight-use-package 'debpaste)
 (straight-use-package 'ix)
 ;; pastbin:1 ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(dash-enable-fontlock t)
+ '(dtk-default-module "KJVA" t)
+ '(dtk-default-module-category "Biblical Texts" t)
+ '(dtk-word-wrap t t)
+ '(highlight-defined-face-use-itself t)
+ '(initial-scratch-message
+   "
+;; Hello! 'Vagner Rener' @ 'Cyberwarrior'.
+;; This buffer is for text that is not saved, and for Lisp evaluation.
+;; To create a file, visit it with \\[find-file] and enter text in its buffer.
+
+")
+ '(nameless-global-aliases nil)
+ '(nameless-private-prefix t)
+ '(org-journal-carryover-items nil t)
+ '(org-journal-date-format "%A, %d %B %Y" t)
+ '(org-journal-dir "~/org~/journal/" t)
+ '(org-journal-enable-agenda-integration t t)
+ '(org-reveal-root "file:/home/vagner/reveal-slides/reveal.js" t)
+ '(telega-notifications-mode t))
