32a6951605a0a06d958bc521c96e95b793ef3f85
Vagner Rener <vagnerrener@gmail.com>
2020-01-12 21:30:43 -0300

    emacs set python3
    	modified:   emacs/.emacs.d/init.el
    	modified:   emacs/.emacs.d/init.org

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -56,7 +56,14 @@
     (setq initial-major-mode 'fundamental-mode)
 
 (custom-set-variables
-                                           '(initial-frame-alist (quote ((fullscreen . maximized)))))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a0feb1322de9e26a4d209d1cfa236deaf64662bb604fa513cca6a057ddf0ef64" default)))
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
                                       ;; for customizing the face (fonts), do:
                                       ;; M-x customize-face RET default RET    
                                       ;;(setq inhibit-startup-screen t)
@@ -997,21 +1004,18 @@ file with `edit-abbrevs`"
 
         (company-ac-setup)
 
-      (custom-set-faces
-          '(company-preview
-            ((t (:foreground "darkgray" :underline t))))
-          '(company-preview-common
-            ((t (:inherit company-preview))))
-          '(company-tooltip
-            ((t (:background "lightgray" :foreground "black"))))
-          '(company-tooltip-selection
-            ((t (:background "steelblue" :foreground "white"))))
-          '(company-tooltip-common
-            ((((type x)) (:inherit company-tooltip :weight bold))
-             (t (:inherit company-tooltip))))
-          '(company-tooltip-common-selection
-            ((((type x)) (:inherit company-tooltip-selection :weight bold))
-             (t (:inherit company-tooltip-selection)))))
+      
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-preview ((t (:foreground "darkgray" :underline t))))
+ '(company-preview-common ((t (:inherit company-preview))))
+ '(company-tooltip ((t (:background "lightgray" :foreground "black"))))
+ '(company-tooltip-common ((((type x)) (:inherit company-tooltip :weight bold)) (t (:inherit company-tooltip))))
+ '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
+ '(company-tooltip-selection ((t (:background "steelblue" :foreground "white")))))
 
   ;;;;;;;;;;;;;;;;;;
 
@@ -3443,6 +3447,8 @@ flycheck-plantuml))
 (setq auto-mode-alist (append '(("\\.pl$" . prolog-mode)
 				("\\.m$" . mercury-mode))
 			       auto-mode-alist))
+
+(setq python-shell-interpreter "/usr/bin/python3")
 
 ;; (use-package quelpa-use-package
 ;;     :straight t
